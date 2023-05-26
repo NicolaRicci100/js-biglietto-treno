35eab7944e882be7c0fc58e3d822f83315559ea7
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-05-26 17:38:43 +0200

    aggiunta paragrafo in html

--- js/script.js
+++ js/script.js
@@ -10,15 +10,18 @@ console.log('età dichiarata ' + userAge);
 
 //calcola prezzo biglietto basandosi sui km scelti dell'utente tenendo in considerazione il costo di 0.21€ al km
 const basePrice = userDistance * 0.21;
+const n = basePrice.toFixed(10);
 
 //applicazione sconti
 if (userAge < 18) {
-    const reducedPrice = (basePrice * 20) / 100;
+    let reducedPrice = (basePrice * 20) / 100;
     console.log('costo biglietto applicando sconto del 20% ' + (basePrice - reducedPrice) + '€');
 } else if (userAge > 65) {
-    const reducedPrice = (basePrice * 40) / 100;
+    let reducedPrice = (basePrice * 40) / 100;
     console.log('costo biglietto applicando sconto del 40% ' + (basePrice - reducedPrice) + '€');
 } else {
-    console.log('costo biglietto senza sconto ' + basePrice + '€');
+    let reducedPrice = basePrice;
+    console.log('costo biglietto senza sconto ' + reducedPrice + '€');
 }
+
 
